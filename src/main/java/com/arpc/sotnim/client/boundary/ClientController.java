7dadec23ccc2ee289331b37b3d89b2c0229e78cd
Arturs Razmuss <arturs.razmuss@gmail.com>
2024-02-11 19:01:30 +0200

    add client controller test

--- src/main/java/com/arpc/sotnim/client/boundary/ClientController.java
+++ src/main/java/com/arpc/sotnim/client/boundary/ClientController.java
@@ -1,9 +1,6 @@
 package com.arpc.sotnim.client.boundary;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/test/java/com/arpc/sotnim/client/boundary/ClientControllerTest.java
+++ src/test/java/com/arpc/sotnim/client/boundary/ClientControllerTest.java
@@ -1,15 +1,11 @@
 package com.arpc.sotnim.client.boundary;
 
-import com.arpc.sotnim.client.entity.ClientRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,7 +17,7 @@ class ClientControllerTest {
     MockMvc mockMvc;
 
     @Test
-    void shouldCreateNewClientWhenValidRequestArrives() throws Exception {
+    void shouldRespondCreatedWhenValidClientCreationRequest() throws Exception {
         mockMvc.perform(post("/clients")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
